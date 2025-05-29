f37565e7ea927ec1749e709f239395ac8ce1a41d
ayoung19 <andyluyoung@gmail.com>
2025-05-28 23:15:42 -0700

    chore: delete all

--- lib/icons/CloudOff.tsx
+++
@@ -1,4 +0,0 @@
-import { CloudOff } from 'lucide-react-native';
-import { iconWithClassName } from './iconWithClassName';
-iconWithClassName(CloudOff);
-export { CloudOff };
--- lib/icons/Edit.tsx
+++
@@ -1,4 +0,0 @@
-import { Edit } from 'lucide-react-native';
-import { iconWithClassName } from './iconWithClassName';
-iconWithClassName(Edit);
-export { Edit };
