e41891593b8fdf6ed3845db470acec3672569958
ayoung19 <andyluyoung@gmail.com>
2025-06-03 02:29:05 -0700

    fix: lint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -30,6 +30,7 @@ export default tseslint.config(
         },
       ],
       "@typescript-eslint/no-require-imports": "off",
+      "@typescript-eslint/no-unused-vars": ["error", { ignoreRestSiblings: true }],
     },
   },
 );
