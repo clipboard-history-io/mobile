6268fcfe01195ab82d19610968469d497fef4a30
ayoung19 <andyluyoung@gmail.com>
2025-06-24 03:59:21 -0700

    fix: workflows

--- scripts/release.sh
+++ scripts/release.sh
@@ -3,6 +3,8 @@
 set -euo pipefail
 trap 'kill $pid_ios $pid_android 2>/dev/null' EXIT
 
+mkdir ./build
+
 (
   eas build --platform ios --profile production --non-interactive --json --wait > ./build/ios.json
   curl --fail -L -H "Authorization: Bearer $EXPO_TOKEN" -o ./build/ios-clipboard-history-io-mobile.ipa "$(jq -r '.artifacts.buildUrl' ./build/ios.json)"
